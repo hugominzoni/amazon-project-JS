d51555e72b0419fa472a297f75c034230ef376e6
Hugo Minzoni <hugominzoni@gmail.com>
2024-10-09 12:40:17 -0400

    make payment summary interactive

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -22,36 +22,36 @@ export function renderPaymentSummary() {
 
     const paymentSummaryHTML = `
         <div class="payment-summary-title">
-            Order Summary
+                Order Summary
             </div>
 
             <div class="payment-summary-row">
-            <div>Items (3):</div>
-            <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
+                <div>Items (3):</div>
+                <div class="payment-summary-money">$${formatCurrency(productPriceCents)}</div>
             </div>
 
             <div class="payment-summary-row">
-            <div>Shipping &amp; handling:</div>
-            <div class="payment-summary-money">$${formatCurrency(shippingPriceCents)}</div>
+                <div>Shipping &amp; handling:</div>
+                <div class="payment-summary-money">$${formatCurrency(shippingPriceCents)}</div>
             </div>
 
             <div class="payment-summary-row subtotal-row">
-            <div>Total before tax:</div>
-            <div class="payment-summary-money">$${formatCurrency(totalBeforeTaxCents)}</div>
+                <div>Total before tax:</div>
+                <div class="payment-summary-money">$${formatCurrency(totalBeforeTaxCents)}</div>
             </div>
 
             <div class="payment-summary-row">
-            <div>Estimated tax (10%):</div>
-            <div class="payment-summary-money">$${formatCurrency(taxCents)}</div>
+                <div>Estimated tax (10%):</div>
+                <div class="payment-summary-money">$${formatCurrency(taxCents)}</div>
             </div>
 
             <div class="payment-summary-row total-row">
-            <div>Order total:</div>
-            <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
+                <div>Order total:</div>
+                <div class="payment-summary-money">$${formatCurrency(totalCents)}</div>
             </div>
 
             <button class="place-order-button button-primary">
-            Place your order
+                Place your order
             </button>`
 
             document.querySelector('.js-payment-summary').innerHTML = paymentSummaryHTML
